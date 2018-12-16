c26784518746e8c2c0f19db3822f90381cbb4c01
mareksuma1985 <msuma@wp.pl>
2018-12-16 19:22:34 +0100

    Initial commit

---
+++ app/src/main/cpp/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := tutorial_udpsink
+LOCAL_SRC_FILES := tutorial_udpsink.c
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog
+include $(BUILD_SHARED_LIBRARY)
+
+GST_V4L2_USE_LIBV4L2 := 1
+
+ifeq ($(TARGET_ARCH_ABI),armeabi)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm
+else ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/armv7
+else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm64
+else ifeq ($(TARGET_ARCH_ABI),x86)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86
+else ifeq ($(TARGET_ARCH_ABI),x86_64)
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86_64
+else
+$(error Target arch ABI not supported: $(TARGET_ARCH_ABI))
+endif
+
+GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build/
+include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_CAPTURE) $(GSTREAMER_PLUGINS_ENCODING) multifile videocrop $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_NET)
+GSTREAMER_EXTRA_DEPS      := gstreamer-video-1.0 gstreamer-player-1.0 gio-2.0 glib-2.0
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
