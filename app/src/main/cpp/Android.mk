3be447e4e61032b51371c3cda58ed52987eb4db2
mareksuma1985 <msuma@wp.pl>
2020-04-11 20:53:50 +0200

    Removed some GStreamer plugins

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -8,16 +8,7 @@ LOCAL_SHARED_LIBRARIES := gstreamer_android
 LOCAL_LDLIBS := -landroid -llog
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE    := gstreamer_udpsink
-LOCAL_SRC_FILES := gstreamer_udpsink.c
-LOCAL_SHARED_LIBRARIES := android_camera gstreamer_android
-LOCAL_LDLIBS := -llog
-include $(BUILD_SHARED_LIBRARY)
-
 APP_PLATFORM := android-6
-
-GST_V4L2_USE_LIBV4L2 := 1
 
 ifndef GSTREAMER_ROOT_ANDROID
 $(error GSTREAMER_ROOT_ANDROID must be defined in /etc/profile!)
@@ -29,16 +20,12 @@ else ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/armv7
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/arm64
-else ifeq ($(TARGET_ARCH_ABI),x86)
-GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86
-else ifeq ($(TARGET_ARCH_ABI),x86_64)
-GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)/x86_64
 else
 $(error Target arch ABI not supported: $(TARGET_ARCH_ABI))
 endif
 
 GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build
 include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
-GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_ENCODING) opensles $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_NET)
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_ENCODING) androidmedia openh264 opensles opengl $(GSTREAMER_PLUGINS_NET)
 GSTREAMER_EXTRA_DEPS      := gstreamer-video-1.0 gstreamer-player-1.0 gio-2.0 glib-2.0
 include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
